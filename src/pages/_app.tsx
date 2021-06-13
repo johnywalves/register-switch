af361a262bd7b527217008b2bdd20468fae9fee0
Johny W. Alves <johnywalves@gmail.com>
2021-06-13 19:33:56 -0300

    :mag: Add SEO Variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,8 +11,44 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
+          name="title"
+          content="Register Switch - Time spent performing tasks"
+        />
+        <meta
           name="description"
           content="Time spent executing tasks in a simple way to enable and disable"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://register-switch.vercel.app/" />
+        <meta
+          property="og:title"
+          content="Register Switch - Time spent performing tasks"
+        />
+        <meta
+          property="og:description"
+          content="Time spent executing tasks in a simple way to enable and disable"
+        />
+        <meta
+          property="og:image"
+          content="https://register-switch.vercel.app/thumbnail.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://register-switch.vercel.app/"
+        />
+        <meta
+          property="twitter:title"
+          content="Register Switch - Time spent performing tasks"
+        />
+        <meta
+          property="twitter:description"
+          content="Time spent executing tasks in a simple way to enable and disable"
+        />
+        <meta
+          property="twitter:image"
+          content="https://register-switch.vercel.app/thumbnail.png"
         />
       </Head>
       <GlobalStyles />
