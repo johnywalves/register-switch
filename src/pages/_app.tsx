1034200dd1e8a2354ae023db6a86e04690538a25
Johny W. Alves <johnywalves@gmail.com>
2021-06-13 16:12:10 -0300

    :wrench: Organize name and description project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,13 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Register Switch - Time spent performing tasks</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Time spent executing tasks in a simple way to enable and disable"
         />
       </Head>
       <GlobalStyles />
